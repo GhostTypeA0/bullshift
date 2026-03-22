ea162b40c38d7eabfef983dbd40152f4d48ae6ab
John Nottom <jrnottom@gmail.com>
2026-03-22 15:52:56 -0500

    Delete Backend - John/bullshift-backend directory

--- Backend - John/bullshift-backend/src/main/java/com/bullshift/bullshift_backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bullshift.bullshift_backend.repository;
-
-import com.bullshift.bullshift_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
